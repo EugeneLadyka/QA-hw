9198314fba3ddb769af5bddba9defcfd65b6e8cb
EugeneLadyka <eugeneladyka@gmail.com>
2020-05-31 13:45:18 +0300

    commit3

--- List.py
+++ List.py
@@ -1,7 +1,7 @@
 from random import randrange
 
-leght = int(input("укажите аргумент опредиляющий длину списка: \n"))
-max = int(input("укажите аргумент максимальное значение списка: \n"))
+leght = int(input("укажите аргумент определяющий длину списка: \n"))
+max = int(input("укажите аргумент, максимальное значение списка: \n"))
 def function(leght, max):
     list=[]
     for i in range (leght):
