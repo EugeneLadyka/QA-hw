2531176fe975c9c2eb48f50f67e56c1e08bbd300
EugeneLadyka <eugeneladyka@gmail.com>
2020-06-14 12:42:26 +0300

    commit5

---
+++ Analysis.py
@@ -0,0 +1,6 @@
+str = "'Денис даёт нам классные задачки, которые помогут нам найти лучшую работу.'"
+symbol = "м"
+{symbol:str.count(symbol) for symbol in set(str)}
+print(str)
+print('символ =', symbol, '\n'
+    'встречается в строке -', str, "-", str.count(symbol), "раз(а).")
